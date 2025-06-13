56898d4c08ce5c53fcc5c81d8c86c242c8625372
setoru <setoru127@gmail.com>
2025-06-13 16:49:40 +0800

    fix setuptools build error

--- huaweicloud_services_server/mcp_server_das/src/server.py
+++ huaweicloud_services_server/mcp_server_das/src/server.py
@@ -3,6 +3,7 @@ from mcp.server.fastmcp import FastMCP
 import logging
 import sys
 from pathlib import Path
+
 # 获取assets目录
 target_dir = Path(__file__).resolve().parent.parent.parent.parent
 # 添加到系统路径
@@ -11,7 +12,6 @@ from assets.utils import sdk_utils
 from mcp_server_das import das_tools
 
 logger = logging.getLogger(__name__)
-
 
 
 def main(transport: str):
@@ -27,6 +27,7 @@ def main(transport: str):
     logger.debug(f"mcp server is running on {transport} mode.")
     logger.debug(target_dir)
     mcp.run(transport=transport)
+
 
 if __name__ == "__main__":
     main(sdk_utils.get_transport())
